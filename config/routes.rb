f07bedc882e621848657233f10c40675dbb99e09
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2016-03-18 11:46:12 +0100

    Basic Navigation with named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+
+  # Static Pages
+  get 'about' => 'static#about'
+  get 'help' => 'static#help'
+
+  # Dashboard
+  get 'home' => 'dashboard#index'
+  get 'jobs' => 'dashboard#jobs'
+  get 'new_job' => 'dashboard#new-job'
+  root 'dashboard#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
