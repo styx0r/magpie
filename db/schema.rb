bca2ca2579cda15ffa05f7ef91fc72ddb6d1f378
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-03-19 16:04:04 +0100

    Adding users password functionality, adding certain user tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160318100739) do
+ActiveRecord::Schema.define(version: 20160319143311) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,9 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User",
+                     email: "user@example.com",
+                     password: "pwSecure", password_confirmation: "pwSecure")
   end
 
   test "user validity?" do
@@ -15,6 +17,62 @@ class UserTest < ActiveSupport::TestCase
 
   test "name present?" do
     @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email present?" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "name too long?" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email too long?" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email valid?" do
+    valid_addresses = %w[user@example.com USER@foo.com A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} is valid"
+    end
+  end
+
+  test "email not valid?" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com test@test..de]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} is not valid"
+    end
+  end
+
+  test "email is unique?" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?, "#{duplicate_user.email} already exists!"
+  end
+
+  test "email is saved in downcase?" do
+    capitalString = "Capital@Uppercase.De"
+    @user.email = capitalString
+    @user.save
+    @user.reload
+    assert_equal @user.email, capitalString.downcase
+  end
+
+  test "password present?" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password minimum length fulfilled?" do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 
