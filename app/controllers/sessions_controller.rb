e2388edccd43d71ab0078a8bb273bec4fc0ea192
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-04-25 13:02:58 +0200

    Account activation finished

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,13 +9,13 @@ class SessionsController < ApplicationController
       if(user.activated?)
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
+        redirect_back_or home_url
       else
         message  = "Account not activated."
         message += "Check your email for the activation link."
         flash[:warning] = message
         redirect_to root_url
-      end 
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/models/user.rb
+++ app/models/user.rb
@@ -44,6 +44,17 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates an account.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
     def downcase_email
