92619d11fb6e098425c16c7c51545450e9d201d6
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2016-04-26 22:36:04 +0200

    Customized job count for user

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,4 +3,9 @@ module DashboardHelper
   def count_jobs(status)
     JobMonitor.where(:status => status).count
   end
+
+  def count_jobs_user(status)
+    user = User.find_by(id: session[:user_id]).name
+    JobMonitor.where(:user => user, :status => status).count
+  end
 end
