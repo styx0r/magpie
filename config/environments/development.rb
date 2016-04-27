b079efc4489f928c8ba6bd5e10831b3e5d9bda10
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-04-27 17:29:04 +0200

    adding email send support, assuming postfix is started: sudo postfix start

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,14 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp #:test
   host = 'localhost:3000' #Localserver
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.smtp_settings = {
+    :address => "localhost",
+    :port => 25,
+    :domain => "mf-www.org",
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
