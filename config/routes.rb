0cd7ff0206ff6c887db2bfe0c8d7ec44e1a88d51
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-04-27 16:31:26 +0200

    adding password Reset formular

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # Static Pages
   get 'about' => 'static#about'
   get 'help' => 'static#help'
@@ -19,6 +23,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
