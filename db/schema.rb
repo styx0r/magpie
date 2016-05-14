61ad0364c1f085febed7daa7d5c0f70267877537
Sebastian Salentin <sebastian.salentin@biotec.tu-dresden.de>
2016-05-14 11:07:44 +0200

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511131940) do
+ActiveRecord::Schema.define(version: 20160513095530) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
@@ -48,6 +48,28 @@ ActiveRecord::Schema.define(version: 20160511131940) do
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
+  create_table "model_jobs", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "priority"
+    t.string   "module"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "status"
+    t.integer  "jobid"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "user"
+    t.string   "job_id"
+    t.string   "name"
+    t.string   "model"
+    t.text     "output"
+    t.text     "resultfiles"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "user_id"
+  end
+
   create_table "user_projects", force: :cascade do |t|
     t.string   "user"
     t.string   "job_id"
@@ -57,6 +79,13 @@ ActiveRecord::Schema.define(version: 20160511131940) do
     t.string   "model"
     t.text     "output"
     t.text     "resultfiles"
+  end
+
+  create_table "userjobs", force: :cascade do |t|
+    t.string   "title"
+    t.string   "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
