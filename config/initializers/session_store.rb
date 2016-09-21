91ceb5d9cac40206036bf81b20baa35a6f7486b8
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-09-21 11:14:04 +0200

    updating to rails 5, still some Deprecations and errors should be fixed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_mfRails_session'
+Rails.application.config.session_store :cookie_store, key: '_mf_rails_session'
