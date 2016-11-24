2f5729394ced90a9ac70ceaea8a5d1ad0624e1da
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-11-24 18:58:41 +0100

    decreasing max size of hashtags, fixing bug with hashtag link in header
    Former-commit-id: fbb6e037f68aaa7db24d0a176990a95d616590b9

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -2,6 +2,6 @@ module HashtagsHelper
   def tagcloudsize(tag)
     # Return an appropriate font size for the tag cloud, normalized by most popular
     max_usage = Hashtag.maximum(:taggings_count)
-    return (6 * (tag.taggings_count / max_usage.to_f)).floor
+    return (5 * (tag.taggings_count / max_usage.to_f)).floor
   end
 end
