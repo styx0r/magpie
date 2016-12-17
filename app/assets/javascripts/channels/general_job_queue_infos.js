c2fadaf205963f17a6daf68393fab8b49a75391e
Christoph Baldow <christoph.baldow@tu-dresden.de>
2016-12-17 09:51:11 +0100

    add image toffle for workload in footer
    Former-commit-id: 6e2f3a5dd12dee3a6b431f115cabc052354fb7c5

--- app/assets/javascripts/channels/general_job_queue_infos.js
+++ app/assets/javascripts/channels/general_job_queue_infos.js
@@ -11,6 +11,28 @@ App.general_job_queue_infos = App.cable.subscriptions.create("GeneralJobQueueInf
     // Called when there's incoming data on the websocket for this channel
     if(document.getElementById("queued_jobs_all")){
       document.getElementById("queued_jobs_all").innerHTML = data.all_queue;
+      document.getElementById("queue_load").innerHTML = data.all_running + "/" + data.worker_number;
+      if (data.worker_number/4 >= data.all_running) {
+        $("#queue-load-pic-yellow").fadeOut();
+        $("#queue-load-pic-orange").fadeOut();
+        $("#queue-load-pic-red").fadeOut();
+        $("#queue-load-pic-black").delay(400).fadeIn();
+      } else if (data.worker_number/2 >= data.all_running) {
+        $("#queue-load-pic-orange").fadeOut();
+        $("#queue-load-pic-red").fadeOut();
+        $("#queue-load-pic-black").fadeOut();
+        $("#queue-load-pic-yellow").delay(400).fadeIn();
+      } else if ((data.worker_number/4 * 3) >= data.all_running) {
+        $("#queue-load-pic-red").fadeOut();
+        $("#queue-load-pic-black").fadeOut();
+        $("#queue-load-pic-yellow").fadeOut();
+        $("#queue-load-pic-orange").delay(400).fadeIn();
+      } else {
+        $("#queue-load-pic-black").fadeOut();
+        $("#queue-load-pic-yellow").fadeOut();
+        $("#queue-load-pic-orange").fadeOut();
+        $("#queue-load-pic-red").delay(400).fadeIn();
+      }
     }
   }
 });
