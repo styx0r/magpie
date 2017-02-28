dee0b37f4a7b12fac15a12709b64b8dc2aabd679
Christoph Baldow <christoph.baldow@tu-dresden.de>
2017-02-28 16:44:39 +0100

    turned off closing micropost side bar when clicking a link (global browser variable)
    Former-commit-id: 721312e3a879e9cbf4e4133f44f1c74681f4c2dd

--- app/assets/javascripts/sidebarCom.js
+++ app/assets/javascripts/sidebarCom.js
@@ -1,11 +1,15 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "650px";
+    document.getElementById("micropostBarID").style.width = "650px";
+    document.getElementById("main").style.marginLeft = "650px";
+    localStorage.setItem("micropostsOpen", true);
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("micropostBarID").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    localStorage.setItem("micropostsOpen", false);
 }
 
 ready = function() {
@@ -13,6 +17,12 @@ ready = function() {
     openNav();
     event.preventDefault();
   });
+  console.log(localStorage.getItem("micropostsOpen"));
+  if(localStorage.getItem("micropostsOpen") == "true"){
+    console.log("test")
+    openNav();
+  }
+
 }
 
 
