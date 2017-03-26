2b3b759a570acc266bb1da85632a61e519d7bb69
Christoph Baldow <christoph.baldow@tu-dresden.de>
2017-03-26 16:02:13 +0200

    closing navbarCommunication on second click
    Former-commit-id: d000648bb5a154aaf40f6b1b79e10123345f060b

--- app/assets/javascripts/sidebarCom.js
+++ app/assets/javascripts/sidebarCom.js
@@ -2,26 +2,31 @@
 function openNav() {
     document.getElementById("micropostBarID").style.width = "650px";
     document.getElementById("main").style.marginLeft = "650px";
-    localStorage.setItem("micropostsOpen", true);
+    //localStorage.setItem("micropostsOpen", true); //if nav should stay open when clicking on other link
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("micropostBarID").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
-    localStorage.setItem("micropostsOpen", false);
+    //localStorage.setItem("micropostsOpen", false); //if nav should stay open when clicking on other link
 }
 
 ready = function() {
   $("#sideBarComOpen").click(function (event){
-    openNav();
     event.preventDefault();
+    if($("#sideBarComOpen")[0].getAttribute("open") != "true"){
+      openNav();
+      $("#sideBarComOpen")[0].setAttribute("open", "true");
+    } else {
+      closeNav();
+      $("#sideBarComOpen")[0].setAttribute("open", "false");
+    }
   });
-  console.log(localStorage.getItem("micropostsOpen"));
-  if(localStorage.getItem("micropostsOpen") == "true"){
-    console.log("test")
-    openNav();
-  }
+  //if nav should stay open when clicking on other link
+  //if(localStorage.getItem("micropostsOpen") == "true"){
+  //  openNav();
+  //}
 
 }
 
