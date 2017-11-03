e03d0c9e816b99f2beefa0e091ec16a1898663b2
Christoph Baldow <christoph.baldow@tu-dresden.de>
2017-11-03 17:13:56 +0100

    fixing micropost controller test, adapting fixtures for microposts and users accordingly, adding old testing users

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -8,23 +8,23 @@ class MicropostsControllerTest < ActionController::TestCase
 
 test "should redirect create when not logged in" do
   assert_no_difference 'Micropost.count' do
-    post :create, micropost: { content: "Lorem ipsum" }
+    post :create, params: { micropost: { content: "Lorem ipsum" } }
   end
-  assert_redirected_to login_url
+  assert_redirected_to root_url
 end
 
 test "should redirect destroy when not logged in" do
   assert_no_difference 'Micropost.count' do
-    delete :destroy, id: @micropost
+    delete :destroy, params: { id: @micropost }
   end
   assert_redirected_to login_url
 end
 
 test "should redirect destroy for wrong micropost" do
-  log_in_as(users(:christoph))
+  log_in_as(users(:admin))
   micropost = microposts(:ants)
   assert_no_difference 'Micropost.count' do
-    delete :destroy, id: micropost
+    delete :destroy, params: { id: micropost }
   end
   assert_redirected_to root_url
 end
